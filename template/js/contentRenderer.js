85bf331f94951d08f6b8003d17fddb4d4eea720a
Raphaël Denault-Lemaire <raphael.dl224@gmail.com>
2026-01-22 15:39:27 -0500

    Center always

--- template/js/contentRenderer.js
+++ template/js/contentRenderer.js
@@ -9,8 +9,10 @@ function processSizedImageShortcode(content) {
 
     return content.replace(shortcodeRegex, (match, src, alt, width, height) => {
         let style = "";
-        if (width) style += `width: ${width};`;
-        if (width !== "100%") style += "display: block; margin-left: auto; margin-right: auto;";
+        if (width) {
+            style += "display: block; margin-left: auto; margin-right: auto; ";
+            style += `width: ${width};`;
+        }
         if (height) style += `height: ${height};`;
         return `<img src="${src}" alt="${alt}" style="${style}">`;
     });
